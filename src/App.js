a8df5f5e40a3da336e36aa86572c3ab5209a5734
Erika Fomich <erikafomich@gmail.com>
2021-10-13 08:38:22 -0700

    removed phone number field from amplify auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Auth, Hub } from 'aws-amplify';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [user, updateUser] = React.useState(null);
+  React.useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => updateUser(user))
+      .catch(() => console.log('No signed in user.'));
+    Hub.listen('auth', data => {
+      switch (data.payload.event) {
+        case 'signIn':
+          return updateUser(data.payload.data);
+        case 'signOut':
+          return updateUser(null);
+        default: return null;
+      }
+    });
+  }, [])
+  if (user) {
+    return (
+      <div>
         <h1>Dream Catch</h1>
-      </header>
-      <AmplifySignOut />
+        <h1>Hello {user.username}</h1>
+        <AmplifySignOut />
+      </div>
+    )
+  }
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <AmplifyAuthenticator>
+        <AmplifySignUp
+          slot="sign-up"
+          formFields={[
+            { type: "username" },
+            {
+              type: "password",
+              label: "Custom Password Label",
+              placeholder: "custom password placeholder"
+            },
+            { type: "email" }
+          ]} 
+        />
+      </AmplifyAuthenticator>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App
